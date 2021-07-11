c177f5413cad1397a8fd6f618bc539e19b9990dc
w311ang <30381083+w311ang@users.noreply.github.com>
2021-07-11 14:50:23 +0800

    Update rmtrash.sh

--- rmtrash.sh
+++ rmtrash.sh
@@ -3,7 +3,9 @@ sudo curl -o /usr/local/bin/rmdirtrash https://raw.githubusercontent.com/Phrozen
 sudo curl -o /usr/local/bin/rmtrash https://raw.githubusercontent.com/PhrozenByte/rmtrash/master/rmtrash
 sudo chmod +x /usr/local/bin/rmtrash /usr/local/bin/rmdirtrash
 echo "alias rm='rmtrash'
-alias rmdir='rmdirtrash'" >> ~/.bashrc
+alias rmdir='rmdirtrash'
+alias sudo='sudo '" >> ~/.bashrc
 sudo bash -c "echo \"alias rm='rmtrash'
-alias rmdir='rmdirtrash'\" >> /root/.bashrc" #为sudo提供回收站
+alias rmdir='rmdirtrash'
+alias sudo='sudo '\" >> /root/.bashrc" #为sudo提供回收站
 source ~/.bashrc
