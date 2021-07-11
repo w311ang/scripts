4a71afe56b7476a0148443ccad4227d99359ba00
w311ang <30381083+w311ang@users.noreply.github.com>
2021-07-11 13:38:11 +0800

    Update rmtrash.sh

--- rmtrash.sh
+++ rmtrash.sh
@@ -1,6 +1,6 @@
 sudo apt install trash-cli curl
-curl -o /usr/local/bin https://raw.githubusercontent.com/PhrozenByte/rmtrash/master/rmdirtrash
-curl -o /usr/local/bin https://raw.githubusercontent.com/PhrozenByte/rmtrash/master/rmtrash
+curl -o /usr/local/bin/rmdirtrash https://raw.githubusercontent.com/PhrozenByte/rmtrash/master/rmdirtrash
+curl -o /usr/local/bin/rmtrash https://raw.githubusercontent.com/PhrozenByte/rmtrash/master/rmtrash
 echo "alias rm='rmtrash'
 alias rmdir='rmdirtrash'
 alias sudo='sudo '" >> ~/.bashrc
