301166fadc2128f7ec35abe92d69d769eb4c0bb1
w311ang <30381083+w311ang@users.noreply.github.com>
2021-07-11 13:33:51 +0800

    Create rmtrash.sh

---
+++ rmtrash.sh
@@ -0,0 +1,5 @@
+curl -o /usr/local/bin https://raw.githubusercontent.com/PhrozenByte/rmtrash/master/rmdirtrash
+curl -o /usr/local/bin https://raw.githubusercontent.com/PhrozenByte/rmtrash/master/rmtrash
+echo "alias rm='rmtrash'
+alias rmdir='rmdirtrash'
+alias sudo='sudo '" >> ~/.bashrc
