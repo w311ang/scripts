ac3de4dfb29612feaed1e96a3fb72e7b63c15e95
w311ang <30381083+w311ang@users.noreply.github.com>
2021-07-11 13:50:03 +0800

    Update rmtrash.sh

--- rmtrash.sh
+++ rmtrash.sh
@@ -1,4 +1,4 @@
-sudo apt install trash-cli curl
+sudo apt install trash-cli
 sudo curl -o /usr/local/bin/rmdirtrash https://raw.githubusercontent.com/PhrozenByte/rmtrash/master/rmdirtrash
 sudo curl -o /usr/local/bin/rmtrash https://raw.githubusercontent.com/PhrozenByte/rmtrash/master/rmtrash
 sudo chmod +x /usr/local/bin/rmtrash /usr/local/bin/rmdirtrash
