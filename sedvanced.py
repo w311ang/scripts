95a9868ef382898694dd33fab9ca4d0cdafc48b0
w311ang <30381083+w311ang@users.noreply.github.com>
2022-11-03 14:12:38 +0800

    Update sedvanced.py

--- sedvanced.py
+++ sedvanced.py
@@ -2,20 +2,24 @@
 import sys
 import re
 
-pattern=sys.argv[1]
-replaceto=sys.argv[2]
-filepath=sys.argv[3]
-try:
-  count=int(sys.argv[4])
-except IndexError:
-  count=0
+def replace(pattern,replaceto,filepath,count):
+  with open(filepath) as f:
+    content=f.read()
 
-with open(filepath) as f:
-  content=f.read()
+  content=re.sub(pattern, replaceto, content, count=count)
 
-content=re.sub(pattern, replaceto, content, count=count)
+  with open(filepath,'w') as f:
+    f.write(content)
 
-with open(filepath,'w') as f:
-  f.write(content)
+  print(filepath)
 
-print(filepath)
+if __main__=='__file__':
+  pattern=sys.argv[1]
+  replaceto=sys.argv[2]
+  filepath=sys.argv[3]
+  try:
+    count=int(sys.argv[4])
+  except IndexError:
+    count=0
+
+  replace(pattern,replaceto,filepath,count)
